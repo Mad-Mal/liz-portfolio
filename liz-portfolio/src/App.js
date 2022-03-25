4faceaf1f0bdb1bfd94fb3c8285eaebc15ca4688
Mallory <mallorydoescoding@gmail.com>
2022-03-24 19:16:31 -0500

    Redid header and finished the base footer to match

--- liz-portfolio/src/App.js
+++ liz-portfolio/src/App.js
@@ -1,12 +1,16 @@
 //Imports
-import './App.css';
 import Navbar from './components/Navbar'
+import Footer  from './components/Footer'
+
+//Style Imports
+import './App.css';
 
 function App() {
   return (
     <div className="outerAppWrapper">
       <div className='innerAppWrapper'>
         <Navbar />
+        <Footer />
       </div>
     </div>
   );
---
+++ liz-portfolio/src/components/Footer.js
@@ -0,0 +1,19 @@
+//Imports
+import React from 'react'
+
+//Style Imports
+import '../styles/footer.css'
+
+const Footer = () => {
+  return (
+    <div className='outerFootWrap'>
+        <div className='innerFootWrap'>
+            <div className='navWrap'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
