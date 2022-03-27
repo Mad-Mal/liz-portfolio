e87fa136923490728fd8e46c9611add764cb49ba
Mallory <mallorydoescoding@gmail.com>
2022-03-27 10:17:05 -0500

    Mapped out all pics and got a decent base styling for them on the gallery page.

--- liz-portfolio/src/components/Gallery.js
+++ liz-portfolio/src/components/Gallery.js
@@ -4,39 +4,51 @@ import React from 'react'
 //Style imports
 import '../styles/gallery.css'
 
+//Image imports
+import butterfly from '../img/lizPortPics/butterfly.jpeg'
+import cherries from '../img/lizPortPics/cherries.jpeg'
+import musicalartist from '../img/lizPortPics/musicalartist.jpeg'
+import snakeandflowers from '../img/lizPortPics/snakeandflowers.jpeg'
+import tiger from '../img/lizPortPics/tiger.jpeg'
+
 let pictures = [
     {
       'index': '0',
-      'pic': 'src/img/lizPortPics/butterfly.jpeg'
+      'pic': butterfly,
+      'alt': 'Hand drawn color picture of a butterfly'
     },
     {
       'index': '1',
-      'pic': 'src/img/lizPortPics/cherries.jpeg'
+      'pic': cherries,
+      'alt': 'Hand drawn color picture of a group of cherries'
     },
     {
       'index': '2',
-      'pic': 'src/img/lizPortPics/musicalartist.jpeg'
+      'pic': musicalartist,
+      'alt': 'Hand drawn color picture of Yungblood'
     },
     {
       'index': '3',
-      'pic': 'src/img/lizPortPics/snakeandflowers.jpeg'
+      'pic': snakeandflowers,
+      'alt': 'Hand drawn picture of a snake with some flowers'
     },
     {
       'index': '4',
-      'pic': 'src/img/lizPortPics/tiger.jpeg'
+      'pic': tiger,
+      'alt': 'Hand drawn picture of a tigers face/head'
     }
 ];
 
 const Gallery = () => {
   return (
     <div className='outerGalleryWrap'>
-      {pictures.map(pic => {
-        <div className='innerGalleryWrap'>
-              <div className='eleWrap' key={pic.index}>
-                {pic.pic}
-              </div>
-        </div>
-      })}
+      <div className='innerGalleryWrap'>
+        {pictures.map(pic => (
+          <div className='eleWrap' key={pic.index}>
+            <img src={pic.pic} alt={pic.alt} className='mappedPics' />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
